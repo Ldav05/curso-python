0e8475bd2730a08ac83cc0417cf8ecea2ac8714a
Luis Gordon <luisdgordon@gmail.com>
2024-06-10 21:55:12 -0500

    Primera clase

---
+++ 00_hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario
+# Hola mundo en Python
+
+print("Hola mundo")
+print('Hola mundo')
+
+"""
+Este es un
+comentario 
+en varias líneas
+"""
+
+'''
+Este es un
+comentario 
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola mundo")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(5+1j)) # Tipo 'cpmplex'
+print(type(True)) # Tipo 'bool'
---
+++ 01_variables.py
@@ -0,0 +1,4 @@
+# Variables
+
+my_variable = "My string variable"  # Es recomendable definir las variables usando snake_case
+print(my_variable)
